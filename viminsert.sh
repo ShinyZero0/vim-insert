fc284547104ee0af2381010f5d60cae325e370c9
ShinyZero0 <105050391+ShinyZero0@users.noreply.github.com>
2023-02-18 02:13:46 +0300

    Fix
    Fix the text not copying with holded modifiers by releasing them with xdotool keyup

--- viminsert.sh
+++ viminsert.sh
@@ -1,5 +1,16 @@
 EDITOR="nvim"
+
 win=$(xdotool getactivewindow)
 xkb-switch -s us
+
 xdotool windowactivate $win
-xdotool key ctrl+c && kitty --class floatwin -- bash -c "xsel -b -o | vipe --suffix txt | xsel -b -i && xdotool windowactivate $win && xdotool key ctrl+v"
+xdotool keyup super
+xdotool keyup alt
+
+xdotool keydown ctrl
+sleep 0.1
+xdotool key c
+sleep 0.1
+xdotool keyup ctrl
+
+kitty --class floatwin -- bash -c "xsel -b -o | vipe --suffix txt | xsel -b -i && xdotool windowactivate $win && xdotool key ctrl+v"
