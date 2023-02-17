24c6879591c7f43ecee28dc1de3e28fc5a44906a
ShinyZero0 <105050391+ShinyZero0@users.noreply.github.com>
2023-02-18 01:44:16 +0300

    add clearmodifiers

--- viminsert.sh
+++ viminsert.sh
@@ -1,10 +1,9 @@
 win=$(xdotool getactivewindow)
 xdotool windowactivate $win
-xdotool key ctrl+c
-sleep 0.1
+xdotool key --clearmodifiers ctrl+c
 xkb-switch -s us
 EDITOR="nvim"
 kitty --class floatwin -- bash -c "xsel -b -o | vipe --suffix txt | xsel -b -i"
 xdotool windowactivate $win
-xdotool key ctrl+v
+xdotool key --clearmodifiers ctrl+v
 
