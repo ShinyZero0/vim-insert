dd9a31eedf312db87cb3c3f01d3fdb87c1b2a808
ShinyZero0 <105050391+ShinyZero0@users.noreply.github.com>
2023-02-18 01:47:12 +0300

    Closing terminal aborts changes now

--- viminsert.sh
+++ viminsert.sh
@@ -3,7 +3,7 @@ xdotool windowactivate $win
 xdotool key --clearmodifiers ctrl+c
 xkb-switch -s us
 EDITOR="nvim"
-kitty --class floatwin -- bash -c "xsel -b -o | vipe --suffix txt | xsel -b -i"
-xdotool windowactivate $win
-xdotool key --clearmodifiers ctrl+v
+kitty --class floatwin -- bash -c "xsel -b -o | vipe --suffix txt | xsel -b -i && xdotool windowactivate $win && xdotool key --clearmodifiers ctrl+v"
+
+
 
