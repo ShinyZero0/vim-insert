4b7e7664c522fe53bbdc74a4ab33e96acfdd4fa3
ShinyZero0 <105050391+ShinyZero0@users.noreply.github.com>
2023-02-18 12:19:07 +0300

    Fix pasting

--- viminsert.sh
+++ viminsert.sh
@@ -4,13 +4,22 @@ win=$(xdotool getactivewindow)
 xkb-switch -s us
 
 xdotool windowactivate $win
+xdotool keyup shift
 xdotool keyup super
 xdotool keyup alt
 
 xdotool keydown ctrl
-sleep 0.1
 xdotool key c
-sleep 0.1
 xdotool keyup ctrl
 
-kitty --class floatwin -- bash -c "xclip -selection clipboard -o | vipe --suffix txt | xclip -selection clipboard -i && xdotool windowactivate $win && xdotool key ctrl+v"
+kitty --class floatwin -- bash -c \
+    "xclip -selection clipboard -o |\
+    vipe --suffix txt |\
+    xclip -selection clipboard -i &&\
+    xdotool windowactivate $win &&\
+    xdotool keyup shift &&\
+    xdotool keyup super &&\
+    xdotool keyup alt &&\
+    xdotool keydown ctrl &&\
+    xdotool key v &&\
+    xdotool keyup ctrl"
