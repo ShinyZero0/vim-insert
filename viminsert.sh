cea579fe30cd8f4c79a67133cca842f51227f752
ShinyZero0 <105050391+ShinyZero0@users.noreply.github.com>
2023-02-18 20:27:50 +0300

    Fix everything

--- viminsert.sh
+++ viminsert.sh
@@ -4,26 +4,12 @@ win=$(xdotool getactivewindow)
 xkb-switch -s us
 
 xdotool windowactivate $win
+
 xdotool keyup shift
 xdotool keyup super
 xdotool keyup alt
-
-xdotool keydown ctrl
-sleep 0.2
-xdotool key c
-sleep 0.2
 xdotool keyup ctrl
 
-kitty --class floatwin -- bash -c \
-    "xclip -selection clipboard -o |\
-    vipe --suffix txt |\
-    xclip -selection clipboard -i &&\
-    xdotool windowactivate $win &&\
-    xdotool keyup shift &&\
-    xdotool keyup super &&\
-    xdotool keyup alt   &&\
-    xdotool keydown ctrl &&\
-    sleep 0.2 &&\
-    xdotool key v &&\
-    sleep 0.2 &&\
-    xdotool keyup ctrl"
+xdotool key ctrl+c
+
+kitty --class floatwin -- bash -c "xclip -selection clipboard -o | vipe --suffix txt | xclip -selection clipboard -i && xdotool windowactivate $win && xdotool keyup shift && xdotool keyup super && xdotool keyup alt && xdotool keyup ctrl && xdotool key ctrl+v"
